96659a0359982033a87cc0c51687952bdfbaf6bc
Henry <mail@henrygressmann.de>
2019-11-11 18:32:07 +0100

    first commit

---
+++ scripts/preinstall.js
@@ -0,0 +1,38 @@
+const pkg = require('./../package.json');
+
+function red(text) {
+	return '\x1b[31m' + text + '\x1b[0m';
+}
+
+function cyan(text) {
+	return '\x1b[36m' + text + '\x1b[0m';
+}
+
+function yellow(text) {
+	return '\x1b[33m' + text + '\x1b[0m';
+}
+
+/**
+ * Do NOT allow using `npm` as package manager.
+ */
+if (process.env.npm_execpath.indexOf('yarn') === -1) {
+	console.log(red('*** canX uses yarn for package management ***'), '\n');
+	console.log(yellow('To install all packages:'));
+	console.log(cyan('$'), 'yarn', '\n');
+	console.log(yellow('To install a new (runtime) package to "dependencies":'));
+	console.log(cyan('$'), 'yarn add --exact [package_name@version]', '\n');
+	console.log(
+		yellow('To install a new (toolset) package to "devDependencies":'),
+	);
+	console.log(cyan('$'), 'yarn add --dev --exact [package_name@version]', '\n');
+	console.log(yellow('To upgrade a package:'));
+	console.log(cyan('$'), 'yarn upgrade --exact [package_name@version]', '\n');
+	console.log(yellow('To remove a package:'));
+	console.log(cyan('$'), 'yarn remove [package_name]', '\n');
+	console.log(
+		yellow('For detailed instructions, see'),
+		cyan(pkg.homepage + '#installation'),
+		'\n',
+	);
+	process.exit(1);
+}
